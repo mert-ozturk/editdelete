d8a498475b54ab9471e229398c93e97fde129b8e
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-12-10 21:10:50 +0300

    islem1

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import "./App.css"
+import data from './mock-data.json'
+import {nanoid} from 'nanoid'
+
+const App = () => {
+  const [contacts,setContacts] = useState(data);
+  const [addFormData,setAddFormData] = useState({
+      fullName:'',
+      address:'',
+      phoneNumber:'',
+      email:''
+  })
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = {...addFormData};
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+
+  }
+const handleAddFormSubmit = (event) => {
+
+  event.preventDefault();
+
+  const newContact = {
+    id: nanoid(),
+    fullName: addFormData.fullName,
+    address: addFormData.address,
+    phoneNumber:addFormData.phoneNumber,
+    email: addFormData.email
+  };
+
+  const newContacts = [...contacts,newContact];
+  setContacts(newContacts);
+
+}
+
+  return (
+    <div className='app-container'>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Adress</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+            {contacts.map((contact)=>(
+               <tr>
+               <td>{contact.fullName}</td>
+               <td>{contact.address}</td>
+               <td>{contact.phoneNumber}</td>
+               <td>{contact.email}</td>
+             </tr>
+             ))}
+        </tbody>
+      </table>
+      <h2>Add a Contact</h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input type='text' name='fullName' required="required" placeholder='Enter Name' onChange={handleAddFormChange}/>
+        <input type='text' name='adress' required="required" placeholder='Enter Adress' onChange={handleAddFormChange} />
+        <input type='text' name='phoneNumber' required="required" placeholder='Enter phoneNumber' onChange={handleAddFormChange} />
+        <input type='email' name='email' required="required" placeholder='Enter Email' onChange={handleAddFormChange} />
+      <button type='submit'>Add</button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default App
